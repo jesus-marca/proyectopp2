df6f81dba1d68cbf1573d2e5aeb494d362992009
braulio-marca <braulio.marca@unmsm.edu.pe>
2023-10-29 01:06:31 -0500

    statics files

--- teaching_blog/settings.py
+++ teaching_blog/settings.py
@@ -152,9 +152,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
+# STATICFILES_DIRS = [
+#     STATIC_DIR,
+# ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
